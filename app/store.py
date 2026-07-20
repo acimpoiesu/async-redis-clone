96d69f9cf602311a7569ac9f3f3a6f93e37ab762
acimpoiesu <megas.alexandru@gmail.com>
2026-07-20 03:34:33 -0400

    feat: implement AOF persistence

--- app/store.py
+++ app/store.py
@@ -1,3 +1,4 @@
+import os
 import time
 import heapq
 import asyncio
@@ -6,15 +7,48 @@ from typing import Optional, Tuple
 MS_PER_SEC = 1000
 EVICTION_TICK_RATE = 0.1
 class DataStore:
-    def __init__(self) -> None:
-        """initializes db state"""
+    def __init__(self, aof_path: str = "database.aof") -> None:
+        """initializes db state and triggers AOF recovery if possible"""
         # maps key -> (value, expiry_timestamp_ms)
         self._store: dict[str, Tuple[str, Optional[float]]] = {}
 
         self._expiry_heap: list[Tuple[float, str]] = []
 
-    def set(self, key: str, value: str, px: Optional[int] = None) -> None:
-        """Inserts or updates key-value pair into db, with optional TTL
+        self.aof_path = aof_path
+        self._restore_from_aof()
+
+    def _append_to_aof(self, command_args:list[str]) -> None:
+        """Logs a mutating command to the disk"""
+        with open(self.aof_path, "a") as f:
+            f.write(" ".join(command_args) + "\n")
+
+    def _restore_from_aof(self) -> None:
+        """Reads AOF to rebuild db state"""
+        if not os.path.exists(self.aof_path):
+            return
+        
+        print("RESTORING database state from {self.aof_path}...")
+
+        with open(self.aof_path, "r") as f:
+            for line in f:
+                parts = line.strip().split()
+                if not parts:
+                    continue
+                cmd = parts[0].upper()
+                if cmd == "SET" and len(parts) >= 3:
+                    key, val = parts[1], parts[2]
+
+                    px_val = None
+                    if len(parts) >= 5 and parts[3].upper() == "PX":
+                        try:
+                            px_val = int(parts[4])
+                        except ValueError:
+                            pass
+                    
+                    self.set(key, val, px=px_val, log_to_aof=False)
+
+    def set(self, key: str, value: str, px: Optional[int] = None, log_to_aof:bool = True) -> None:
+        """Inserts or updates key-value pair into db, with optional TTL and logging
 
         Args:
             key (str): identifier for the payload
@@ -26,6 +60,12 @@ class DataStore:
             expiry_time = (time.time() * MS_PER_SEC) + px
             heapq.heappush(self._expiry_heap, (expiry_time, key))
         self._store[key] = (value, expiry_time)
+
+        if log_to_aof:
+            command = ["SET", key, value]
+            if px is not None:
+                command.extend(["PX", str(px)])
+            self._append_to_aof(command)
 
     def get(self, key:str) -> Optional[str]:
         """Retrieves a value by key
